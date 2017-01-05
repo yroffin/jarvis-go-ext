bb6239b0103037735f8a104ee954038133d2ecd9
Yannick Roffin <yroffin@gmail.com>
2017-01-05 23:20:53 +0100

    Correction du script

--- launch-dio.cmd
+++ launch-dio.cmd
@@ -1,1 +1,1 @@
-jarvis-go-ext.exe start --jarvis.option.dio true --jarvis.option.mongodb 192.168.1.47
+jarvis-go-ext.exe start --jarvis.option.wiringpi true --jarvis.option.dio true --jarvis.option.mongodb 192.168.1.73
